4d9e0ccb410743c8f98814937f61d5f77f07a8e2
omarakamal <omaraakamal@gmail.com>
2025-02-10 18:36:14 +0300

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import {Routes, Route} from 'react-router'
+import Students from './components/Students'
+
+// Step 2: import Route and Routes from "react-router"
 
 function App() {
 
   return (
     <>
    
+      <Routes>
+        <Route path = "/students" element = {<Students/>}  />
+      </Routes>
+
+
+      
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter} from "react-router"
+
+// Step 1: import BrowserRouter from "react router" and wrap my application with it
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+ 
 )
